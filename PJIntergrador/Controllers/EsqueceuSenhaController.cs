170afd904488cf99e9a0588944049a753488a9b3
Ryan7772 <ryanwillrich730@gmail.com>
2023-11-29 21:28:30 -0300

    Controller

---
+++ PJIntergrador/Controllers/EsqueceuSenhaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PJIntergrador.Controllers
+{
+    public class EsqueceuSenhaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
